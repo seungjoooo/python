f65dc3305862d639929767385f54e3de53e0fd90
seungjoo <pool9604@naver.com>
2022-01-07 01:34:07 +0900

    learn variable in python

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,16 @@
-print("hello world")
-print(5>10)
-print(3<10)
-print(3<=10)
-print(True)
-print(False)
-print(not True)
-print(not False)
-print(not (3<10))
+age = 2
+animal = "강아지"
+name = "연탄이"
+hobby = "산책"
+is_adult= age >= 3
+print("우리집"+animal+"의 이름은 "+name)
+print(name+"는 "+str(age)+"입니다") #정수형, 불형을 문장형으로 바꿔주는 방식
+print(name + "은 어른일까요?"+str(is_adult))
+
+name="밍밍이"
+print("우리집",animal,"의 이름은 ",name)
+age = 5
+is_adult= age >= 3
+print(name,"는 ",age,"입니다") #정수형, 불형을 문장형으로 바꿔주는 방식
+print(name ,"은 어른일까요?",is_adult)
+
