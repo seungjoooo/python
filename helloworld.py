f57a2ceed64b422a7951ce886e3ade428a9a79f5
seungjoo <pool9604@naver.com>
2022-01-07 01:47:09 +0900

    learn how to explain code

--- helloworld.py
+++ helloworld.py
@@ -7,10 +7,19 @@ print("우리집"+animal+"의 이름은 "+name)
 print(name+"는 "+str(age)+"입니다") #정수형, 불형을 문장형으로 바꿔주는 방식
 print(name + "은 어른일까요?"+str(is_adult))
 
-name="밍밍이"
-print("우리집",animal,"의 이름은 ",name)
-age = 5
-is_adult= age >= 3
-print(name,"는 ",age,"입니다") #정수형, 불형을 문장형으로 바꿔주는 방식
-print(name ,"은 어른일까요?",is_adult)
+# name="밍밍이"
+# print("우리집",animal,"의 이름은 ",name)
+# age = 5
+# is_adult= age >= 3
+# print(name,"는 ",age,"입니다") 
+# print(name ,"은 어른일까요?",is_adult)
+
+#주석 사용 방법 한줄
+'''
+
+여러줄 주석 방법
+1. 작은따음표 3개 이용
+2. 영역 설정 후 ctrl /해서 한번에 선택 영역 주석 설정 & 해제 
+
+'''
 
